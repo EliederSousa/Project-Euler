8cdd06fa6996239372f9e3ac59da7b834717ea1c
EliederSousa <eliedersousa@gmail.com>
2020-08-07 14:22:42 -0300

    Update 016.py

--- 016.py
+++ 016.py
@@ -1,6 +1,19 @@
+## 
+#  016.py
+#  
+#  Copyright (c) 2020, Elieder Sousa
+#  eliedersousa<at>gmail<dot>com
+#  
+#  Distributed under the MIT license.
+#  
+#  @date     07/08/20
+#
+#  @brief    "Power digit sum"
+#  			2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#  			What is the sum of the digits of the number 2^1000?
+##
+
 # Simple one-line answer. It even looks like cheating.
 # If you see an other file with the same number, take a look on it.
 # Maybe I did the hard implementation of the same thing.
 sum(map(int, str(2**1000)))
-
-
