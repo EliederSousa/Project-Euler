d7669f29b8de7c73fa9bb213661353b9faf1ad58
EliederSousa <eliedersousa@gmail.com>
2020-08-07 14:18:32 -0300

    Add files via upload

---
+++ 016.py
@@ -0,0 +1,6 @@
+# Simple one-line answer. It even looks like cheating.
+# If you see an other file with the same number, take a look on it.
+# Maybe I did the hard implementation of the same thing.
+sum(map(int, str(2**1000)))
+
+
