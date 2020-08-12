b5b7e286aab3430a798a52ad30d83038e3ab7273
EliederSousa <eliedersousa@gmail.com>
2020-08-11 21:33:44 -0300

    Update 020.py

--- 020.py
+++ 020.py
@@ -9,10 +9,10 @@
 #  @date     11/08/20
 #  
 #  @brief    "Factorial digit sum"
-#  			n! means n × (n − 1) × ... × 3 × 2 × 1
-#  			For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
-#  			and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
-#  			Find the sum of the digits in the number 100!
+#  		n! means n × (n − 1) × ... × 3 × 2 × 1
+#  		For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+#  		and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#  		Find the sum of the digits in the number 100!
 ##
 
 import math
